5e67f718d2f76893ecee95b0069513749b549ca1
Kazuyoshi Goto <aimluck.goto@gmail.com>
2014-06-18 16:35:08 +0900

    モック更新

---
+++ mock_009/config.rb
@@ -0,0 +1,10 @@
+http_path = "/"
+css_dir = "themes/default/css"
+sass_dir = "themes/default/sass"
+images_dir = "themes/default/img"
+javascripts_dir = "themes/default/js"
+
+output_style = :expanded
+#expanded/nexted/compact/compressed
+relative_assets = true
+line_comments = false

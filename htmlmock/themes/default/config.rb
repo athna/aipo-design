7fc42031fda781ea3b8af3bee09ceb7ba2895d26
Kazuyoshi Goto <aimluck.goto@gmail.com>
2014-06-30 16:11:48 +0900

    refs 32688

---
+++ htmlmock/themes/default/config.rb
@@ -0,0 +1,10 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+output_style = :expanded
+#expanded/nexted/compact/compressed
+relative_assets = true
+line_comments = false

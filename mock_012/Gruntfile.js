2786f66750e4f3ef42ea136e640651e768e82118
Kazuyoshi Goto <aimluck.goto@gmail.com>
2014-07-04 10:10:39 +0900

    gruntfile.js update

--- mock_012/Gruntfile.js
+++ mock_012/Gruntfile.js
@@ -15,34 +15,24 @@ module.exports = function (grunt) {
 				}
 			}
 		},
-		//coffee: {
-		//	compile: {
-		//		files: {
-		//			'themes/default/js/index.js': ['themes/default/coffee/*.coffee']
-		//		}
-		//	}
-		//},
-		//jshint: {
-		//	files: ['js/index.js']
-		//},
 		watch: {
 			files: ['themes/default/sass/*.scss'],
 			tasks: ['compass']
 		}
 	});
-	//grunt.loadNpmTasks('grunt-contrib-compass');
-	//grunt.loadNpmTasks('grunt-contrib-compress');
-	//grunt.loadNpmTasks('grunt-contrib-csslint');
-	//grunt.loadNpmTasks('grunt-contrib-cssmin');
-	//grunt.loadNpmTasks('grunt-contrib-htmlmin');
-	//grunt.loadNpmTasks('grunt-contrib-sass');
-	//grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	var taskName;
-	for (taskName in pkg.devDependencies) {
-		if (taskName.substring(0, 6) == 'grunt-') {
-			grunt.loadNpmTasks(taskName);
-		}
-	}
+	//var taskName;
+	//for (taskName in pkg.devDependencies) {
+	//	if (taskName.substring(0, 6) == 'grunt-') {
+	//		grunt.loadNpmTasks(taskName);
+	//	}
+	//}
 	grunt.registerTask('default', ['watch']);
 };
